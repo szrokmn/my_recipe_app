e40f7e2eb9fc3990bdc1531f8f1d053ef93c65e6
Sezer Ökmen <szrokmn34@gmail.com>
2023-01-04 16:17:05 +0300

    first commit

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
