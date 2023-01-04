eebec35e8c3e06206993518d49d19cc8c98c8e94
Sezer Ökmen <szrokmn34@gmail.com>
2023-01-04 19:43:36 +0300

    Login finished

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -9,20 +9,30 @@ import {
   StyledInput,
 } from "./Login.style";
 import meal from "../../assets/meal.svg";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
 
+  const userInfo = {
+    username:"admin"
+  }
 
-  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sessionStorage.setItem("user", JSON.stringify(userInfo))
+    navigate(-1);
+  };
+
   return (
     <LoginContainer>
       <FormContainer>
         <StyledImg src={meal} />
         <Header>{"<CW/>"}Recipe</Header>
-        <StyledForm onSubmit = {handleSubmit}>
+        <StyledForm onSubmit={handleSubmit}>
           <StyledInput placeholder="Enter Username" type="text" />
-          <StyledInput placeholder="Enter Password" type="password"/>
-          <StyledButton type = "submit">Login</StyledButton>
+          <StyledInput placeholder="Enter Password" type="password" />
+          <StyledButton type="submit">Login</StyledButton>
         </StyledForm>
       </FormContainer>
     </LoginContainer>
