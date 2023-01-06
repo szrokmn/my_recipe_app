725d128f0400549f3c8cb7231d472613bf3f8cc7
Sezer Ökmen <szrokmn34@gmail.com>
2023-01-06 11:56:49 +0300

    url, app_ıd, app_key added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 
 const Home = () => {
+
+  const APP_ID = "020f0e58";
+  const APP_KEY = "7384df4667cdba1b0b48c1a225cd3714"
+  
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
+
+
   return (
     <div>
       Home
